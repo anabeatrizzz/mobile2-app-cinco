74d10ac346678ba70be8d2ff70a5c45b5e4dd391
Ana Beatriz <anabeatriz.augusto06@yahoo.com>
2022-02-09 17:21:21 -0300

    test external link onPress

--- App.test.js
+++ App.test.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { render } from '@testing-library/react-native';
-
+import { Linking } from "react-native"
+import { render, fireEvent } from '@testing-library/react-native';
 import App from './App';
 
-const tree = renderer.create(<App />)
+jest.mock("react-native/Libraries/Linking/Linking", () => ({
+	openURL: jest.fn(() => Promise.resolve("mockResolve")),
+}))
 
 describe('<App />', () => {
   it('shows the external link', () => {
     const { getByTestId } = render(<App />)
     expect(getByTestId('external-link-text')).not.toBeNull()
+  })
+
+  it('handles external link onPress', () => {
+    const { getByTestId } = render(<App />)
+    fireEvent.press(getByTestId('external-link-text'))
+    expect(Linking.openURL).toHaveBeenCalledTimes(1)
   })
 })
